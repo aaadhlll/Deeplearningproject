35acf0afb1c6a97501bc4f0dfe6ea9c9bc01acd2
aaadhlll <aaaadhllkv@gmail.com>
2024-03-13 20:24:59 +0530

    requirements added

--- template.py
+++ template.py
@@ -24,7 +24,8 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 
 
 ] #defining the List of files to be created 
